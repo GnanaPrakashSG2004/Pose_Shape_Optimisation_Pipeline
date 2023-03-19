568107ddf47cf960188fd3468c4bb05be42946fb
Gnana Prakash <gnanaprakashpunnavajhala@gmail.com>
2023-03-19 13:08:18 +0530

    Changed errors in q2 and variable b to w in q1

--- Task_2/q1.m
+++ Task_2/q1.m
@@ -8,14 +8,14 @@ title('Before adjustment');
 
 % Calculating the dimensions of the mean shape
 l = max(M(:, 2)) - min(M(:, 2));
-b = max(M(:, 1)) - min(M(:, 1));
+w = max(M(:, 1)) - min(M(:, 1));
 h = max(M(:, 3)) - min(M(:, 3));
 
-fprintf('Dimensions of the vehicle:\n\nLength: %f\nBreadth: %f\nHeight: %f\n\n', l, b, h);
+fprintf('Dimensions of the vehicle:\n\nLength: %f\nWidth: %f\nHeight: %f\n\n', l, w, h);
 
 % Scaling the dimensions of the mean shape
 M(:, 2) = M(:, 2) * 3.8600 / l;
-M(:, 1) = M(:, 1) * 1.6362 / b;
+M(:, 1) = M(:, 1) * 1.6362 / w;
 M(:, 3) = M(:, 3) * 1.5208 / h;
 
 nexttile;
--- Task_2/q2.m
+++ Task_2/q2.m
@@ -1,5 +1,14 @@
 % Changing the mean shape vector
 M = readmatrix('meanShape.txt');
+
+l = max(M(:, 2)) - min(M(:, 2));
+w = max(M(:, 1)) - min(M(:, 1));
+h = max(M(:, 3)) - min(M(:, 3));
+
+M(:, 2) = M(:, 2) * 3.8600 / l;
+M(:, 1) = M(:, 1) * 1.6362 / w;
+M(:, 3) = M(:, 3) * 1.5208 / h;
+
 M = -M;
 M(:, [2, 3]) = M(:, [3, 2]);
 writematrix(M, 'updatedMeanShape.txt', 'Delimiter', 'space');
@@ -8,6 +17,11 @@ visualizeWireframe3D(M');
 
 % Changing the deformation vectors
 DV = readmatrix('vectors.txt');
+
+DV(:, 2) = DV(:, 2) * 3.8600 / l;
+DV(:, 1) = DV(:, 1) * 1.6362 / w;
+DV(:, 3) = DV(:, 3) * 1.5208 / h;
+
 DV = -DV;
 for swapCol = 2:3:41
     DV(:, [swapCol, swapCol + 1]) = DV(:, [swapCol + 1, swapCol]);
